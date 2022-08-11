afa13643b5450c40149688eb3c9064c2dd4b626f
anshurw <anshurw@gmail.com>
2022-08-11 09:25:22 -0700

    update

--- Assets/scripts/Crossheir.cs
+++ Assets/scripts/Crossheir.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 
 public class Crossheir : MonoBehaviour
@@ -12,7 +13,8 @@ public class Crossheir : MonoBehaviour
     public float _moveSpeed = 10f;
     public int noofshots;
     public soundmanager sm;
-
+    public int _soundon;
+    
     
     // Use this for initialization
     void Start()
@@ -47,6 +49,29 @@ public class Crossheir : MonoBehaviour
         if (GameObject.FindGameObjectWithTag("LEVELMAN").GetComponent<LevelManager>().CurrentLevel == 49&& noofshots>100)
         {
             Instantiate(canv, transform.position, quaternion.identity);
+        }
+
+        if (EventSystem.current.currentSelectedGameObject != null)
+        {
+            if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject.name == "music" &&
+                _soundon == 0)
+            {
+                print("Sound off");
+                sm.audiosf();
+                AudioListener.volume = 0;
+                _soundon = 1;
+                PlayerPrefs.SetInt(nameof(_soundon), 1);
+
+            }
+            else if (Input.GetMouseButtonDown(0) && EventSystem.current.currentSelectedGameObject.name == "music" &&
+                     _soundon == 1)
+            {
+                print("sound on");
+                sm.audiosf();
+                _soundon = 0;
+                AudioListener.volume = 1;
+                PlayerPrefs.SetInt(nameof(_soundon), 0);
+            }
         }
     }
 
--- Assets/scripts/soundmanager.cs
+++ Assets/scripts/soundmanager.cs
@@ -35,4 +35,6 @@ public class soundmanager : MonoBehaviour
     {
         blast.Play();
     }
+    
+    
 }
